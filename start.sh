0d34a680475fb5feec4a56ede2dd70d09c13ac50
unknown <abdirahmanmuuse033@gmail.com>
2026-06-26 09:40:04 +0300

    Remove cached Sail bootstrap files, regenerate package manifest on start

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "==> Setting up Laravel..."
 
+# Regenerate package manifest (removes dev-only providers like Sail)
+php artisan package:discover --ansi
+
 # Generate app key if not set
 if [ -z "$APP_KEY" ] || [ "$APP_KEY" = "base64:GENERATE_WITH_php_artisan_key:generate" ]; then
   php artisan key:generate --force
